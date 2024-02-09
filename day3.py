c19ab6d91a665a8c9a3d9c0201743b67bd4bdc0a
mav1ng <m.spreng@stud.uni-heidelberg.de>
2024-02-09 19:51:34 +0100

    add solution day3 part 2

--- day3.py
+++ day3.py
@@ -10,9 +10,11 @@ class Day3(aoc.Day):
             lines = file.read()
         self.input = lines.split("\n")
 
+
     def run(self):
         print(self.find_numbers())
-
+        self.reset_input()
+        print(self.find_gear())
 
     def find_numbers(self):
         rows = len(self.input)
@@ -33,8 +35,40 @@ class Day3(aoc.Day):
         print("\n", "\n".join(self.input))
         return total
 
+    def reset_input(self):
+        self.input = []
+        with open("data/aoc/3.txt", 'r') as file:
+            lines = file.read()
+        self.input = lines.split("\n")
 
-    def matchNumber(self, row, index):
+    def find_gear(self):
+        rows = len(self.input)
+        columns = len(self.input[0])
+        symbols = []
+        total = 0
+        gear_ratios = []
+        counter = 0
+        for i in range(0, len(self.input) - 1):
+            for k in range(len(self.input[0])):
+                if self.input[i][k] == "*":
+                    self.reset_input()
+                    for m in range(-1, 2, 1):
+                        for n in range(-1, 2, 1):
+                            if 0 <= i + n < rows and 0 <= k + m < columns:
+                                if counter > 2:
+                                    break
+                                res = self.matchNumber(row=i + n, index=k + m, replace=True)
+                                if res != 0:
+                                    gear_ratios.append(res)
+                                    counter += 1
+                    if counter == 2:
+                        total += gear_ratios[0] * gear_ratios[1]
+                    counter = 0
+                    gear_ratios = []
+        return total
+
+
+    def matchNumber(self, row, index, replace=True):
         if not self.input[row][index].isdigit():
             return 0
         line = self.input[row]
@@ -51,9 +85,10 @@ class Day3(aoc.Day):
             else:
                 break
         ret = line[left:right + 1]
-        change = list(self.input[row])
-        change[left:right + 1] = len(ret) * ["."]
-        self.input[row] = "".join(change)
+        if replace:
+            change = list(self.input[row])
+            change[left:right + 1] = len(ret) * ["."]
+            self.input[row] = "".join(change)
         return int("".join(ret))
 
 
