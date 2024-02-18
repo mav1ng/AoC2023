c4c9a475303bf6c4aaa7f1c15da01f5e26d35464
mav1ng <m.spreng@stud.uni-heidelberg.de>
2024-02-18 17:45:04 +0100

    add day 13 part 1 + part 2

--- adventofcode.py
+++ adventofcode.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-from collections import deque
-import re
 
 
 class Day:
@@ -11,35 +7,6 @@ class Day:
     def run(self):
         raise NotImplementedError
 
-
-class Day13(Day):
-    def __init__(self):
-        super().__init__()
-        with open("data/aoc/13.txt", 'r') as file:
-            lines = file.read()
-
-        self.input = []
-        for line in lines.split(sep="\n\n"):
-            self.input.append(line.split(sep="\n"))
-
-    def check_vertical(self):
-        ret = []
-        for patch in self.input:
-            patch = np.array(patch)
-            print(patch[:, 0:3])
-            breakpoint()
-            ismirror = None
-            patchlen = len(patch[0])
-            for vmirror in range(1, patchlen - 1):
-                width = min(len(patch[0][:vmirror]), len(patch[0][vmirror:]))
-                print(patch[:][max(0, vmirror - width):vmirror], patch[:][vmirror:min(patchlen, vmirror + width)])
-                if (patch[:][max(0, vmirror - width):vmirror] == patch[:][vmirror:min(patchlen, vmirror + width)]).all():
-                    ismirror = vmirror
-            ret.append(ismirror)
-
-    def run(self):
-        self.check_vertical()
-        pass
 
 class Day19(Day):
     def __init__(self):
